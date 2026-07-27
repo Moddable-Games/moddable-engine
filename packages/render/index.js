2846c4ef189433ac998d50c8b18ac820d2491f40
Mark Smalley <mark@smalley.my>
2026-07-27 15:16:06 +0100

    Export buildPieceImages and attachPieceImages from render package
    Consumers (like the tools Worker) need to build piece image maps from
    the gallery without going through the full board-renderer pipeline.

--- packages/render/index.js
+++ packages/render/index.js
@@ -1,2 +1,3 @@
 export { createBoardRenderer } from './src/board-renderer.js'
 export { serializeLayout } from './src/serialize-layout.js'
+export { buildPieceImages, attachPieceImages } from './src/render-engine.js'
