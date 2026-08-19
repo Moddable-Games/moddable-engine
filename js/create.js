7d2f40c81d0b444e6ae29d56a048221f0fc5c04f
Mark Smalley <mark@smalley.my>
2026-08-19 16:43:57 +0100

    Bump version 1.0.22

--- js/create.js
+++ js/create.js
@@ -231,7 +231,7 @@ function reanchorPlacement(oldRows, oldCols, rows, cols) {
 }
 
 async function loadGallery() {
-  try { galleryIndex = await fetch('../pieces/gallery-index.json?v=1.0.21').then(r => r.json()) }
+  try { galleryIndex = await fetch('../pieces/gallery-index.json?v=1.0.22').then(r => r.json()) }
   catch { galleryIndex = [] }
 }
 
