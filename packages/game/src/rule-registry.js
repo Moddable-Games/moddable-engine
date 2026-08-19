0370b53f17b98284caa3c658dda8cde6e187b7e0
Mark Smalley <mark@smalley.my>
2026-08-19 13:39:51 +0100

    #136: Consolidate competing rule registries
    packages/game/src/rule-registry.js redefined createRuleRegistry despite
    already importing from packages/rule. Now re-exports the canonical
    implementation from packages/rule instead of maintaining a duplicate.

--- packages/game/src/rule-registry.js
+++ packages/game/src/rule-registry.js
@@ -1,32 +1,7 @@
-import { composeRules } from '../../rule/index.js'
+import { composeRules, createRuleRegistry } from '../../rule/index.js'
 import { resolveOrder, validateTopologyNeeds } from '../../rule/index.js'
 
-export function createRuleRegistry() {
-  const factories = new Map()
-
-  function register(id, factory) {
-    if (typeof factory !== 'function') {
-      throw new Error(`Rule "${id}" must be registered with a factory function`)
-    }
-    factories.set(id, factory)
-  }
-
-  function create(id, config = {}) {
-    const factory = factories.get(id)
-    if (!factory) {
-      throw new Error(`No rule factory registered for "${id}"`)
-    }
-    const rule = factory(config)
-    if (!rule.id) rule.id = id
-    return rule
-  }
-
-  function has(id) {
-    return factories.has(id)
-  }
-
-  return { register, create, has }
-}
+export { createRuleRegistry }
 
 export function resolveRuleOverrides(baseRules, overrides, registry) {
   const active = [...baseRules]
