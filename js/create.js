c625a3886351eaaa45e3368c34d35d351f64c568
Mark Smalley <mark@smalley.my>
2026-09-25 13:47:26 +0100

    v1.0.41

--- js/create.js
+++ js/create.js
@@ -292,7 +292,7 @@ function reanchorPlacement(oldRows, oldCols, rows, cols) {
 }
 
 async function loadGallery() {
-  try { galleryIndex = await fetch('../pieces/gallery-index.json?v=1.0.40').then(r => r.json()) }
+  try { galleryIndex = await fetch('../pieces/gallery-index.json?v=1.0.41').then(r => r.json()) }
   catch { galleryIndex = [] }
 }
 
